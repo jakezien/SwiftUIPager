3e445764c366bfb4c0d9a19c4e8bf801aa548cdd
jakezien <jakezien@hey.com>
2023-02-26 21:01:57 -0500

    kill all the warnings

--- Sources/SwiftUIPager/PagerContent.swift
+++ Sources/SwiftUIPager/PagerContent.swift
@@ -470,7 +470,8 @@ extension Pager.PagerContent {
         }
 
         // #194 AnimatableModifier symbol not found in iOS 13.0 and iOS 13.1
-        if #available(iOS 13.2, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
+//        if #available(iOS 13.2, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
+        if #available(iOS 13.2, tvOS 13.0, watchOS 6.0, *) {
             // Do nothing
         } else if page != newPage {
             self.pagerModel.pageIncrement = 0
